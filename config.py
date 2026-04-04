37e5a56d5b48c48220000eccb5310eb28a14078f
Trotte Karlsson <trotte.karlsson1@gmail.com>
2026-04-04 21:12:30 +0200

    fix: encode password special characters in connection URL

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 import logging
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -23,8 +24,9 @@ DB_NAME     = os.getenv("DB_NAME")
 
 
 def get_engine():
+    password = quote_plus(DB_PASSWORD)
     return create_engine(
-        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        f"postgresql://{DB_USER}:{password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 
 
